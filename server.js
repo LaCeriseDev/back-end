76e34155dbd86fbcfcc79ef5a36616795f9526ad
LaCeriseDev <zaniniromain.pro@gmail.com>
2022-05-08 14:38:07 +0200

    remove wrong files

--- server.js
+++ server.js
@@ -1,8 +1,5 @@
 //modules
 const express = require('express')
-const cookieParser = require('cookie-parser')
-const userRoutes = require('./routes/user.routes')
-const {checkUser, requireAuth} = require('./middleware/auth.middleware')
 
 require('dotenv').config({path:'./config/.env'})
 require('./config/db')
@@ -12,16 +9,10 @@ const app = express()
 // middleware
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
-app.use(cookieParser())
 
-// jwt
-app.get('*', checkUser)
-app.get('/jwtid', requireAuth, (req, res) => {
-  res.status(200).send(res.locals.user._id)
 })
 
 // route
-app.use('/api/user', userRoutes)
 
 // server
 app.listen(process.env.PORT, () => {
